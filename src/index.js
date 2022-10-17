5d1cc879e3f8ebda6b107100c28dd9129af8381b
unknown <hindmuhamed.178@gmail.com>
2022-10-17 19:33:52 +0300

    Complete Assigment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
+    const formInput = document.querySelector('form')
+    formInput.addEventListener('submit', (event) => {
+      event.preventDefault()
+      const value = document.querySelector('input#searchByID').value
   
-}
-
+      fetch(`http://localhost:3000/movies/${value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector('section#movieDetails h4')
+          const summary = document.querySelector('section#movieDetails p')
+          title.innerText = data.title
+          summary.innerText = data.summary
+        })
+    })
+  }
+  
 document.addEventListener('DOMContentLoaded', init);
